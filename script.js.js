e629917d0b67bc7e1ea45dd05727a82dca6a8395
Orhoro Daniel <122745729+Orhorodaniel@users.noreply.github.com>
2023-10-29 14:46:05 +0100

    Delete script.js.js

--- script.js.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.querySelector ('.toggle');
-const sidebar = document.querySelector ('.sidebar');
-
-menuToggle.onclick = () => {
-    menuToggle.classList.toggle('active');
-    sidebar.classList.toggle('active');
-};
